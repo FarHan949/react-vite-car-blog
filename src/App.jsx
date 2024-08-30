2c8b1ad414873a87f53ae3a456149f0348de50b0
FarHan949 <uddinfarhan949@gmail.com>
2024-08-30 22:33:02 +0600

    adding fake data and used fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { createBrowserRouter } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const route = createBrowserRouter([
-    {
+
+  const [car, setCar] = useState([])
+
+  useEffect(()=> {
+    fetch('car.json')
+    .then( res => res.json())
+    .then( data => setCar(data))
+  },[])
+
+  // const route = createBrowserRouter([
+  //   {
       
-    }
-  ])
+  //   }
+  // ])
 
   return (
-    <>
-      
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Vite + React {car.length}</h1>
+    </div>
   )
 }
 
